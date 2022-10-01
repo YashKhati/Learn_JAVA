973a4c2a8260562c99c8fc944b5fc55c59c7413c
Yash Khati <103344945+YashKhati@users.noreply.github.com>
2022-10-01 17:50:16 +0530

    Create CleelingOfNumber.java

---
+++ Practice/BinarySearch/CleelingOfNumber.java
@@ -0,0 +1,35 @@
+public class CellingOfNumber
+{
+        public static void main(String[] args)
+        {
+            int[] arr={1,3,5,7,10,14};
+            int target=0;
+            int ans= Celling(arr,target);
+            System.out.println(ans);
+        }
+        static  int Celling(int[] ar, int target)
+        {
+            if(target>ar.length-1)
+                return  -1;
+            int start=0;
+            int end=ar.length - 1;
+            while (start<=end)
+            {
+                int mid=start+(end-start)/2;
+                if(ar[mid]==target)
+                {
+                    return mid;
+                }
+                else if(target<ar[mid])
+                {
+                    end=mid-1;
+                }
+                else if (target>ar[mid])
+                {
+                    start = mid + 1;
+                }
+            }
+            return start;
+        }
+}
+
